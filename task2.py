71819f44ff853df78ad25ae3aae65b95a8ebdf72
jaydengonzalezgu347-hue <jayden.gonzalezgu347@students.cmcss.net>
2025-12-12 12:50:57 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,14 +1,14 @@
 balance = starting_balance
-balance -= withdrawl_amount
+balance -= withdrawal_amount
 # Subtract ATM fee
-balance-= atm_fee - balance
-total_montly_fees = balance % 30
+balance-= atm_fee
+total_monthly_fee = balance % 30
 # Calculate and subtract total monthly fees
-balance -= total_monthly_fees - balance
+balance -= total_monthly_fee
 # Calculate full $20 bills and remaining dollars
-full_twenties = balance // total_monthly_fees
+full_twenties = balance // total_monthly_fee
 # Display results with f-strings
 print(f"Account Holder: {account_holder}")
-print(f"Remanining Balance: ")
-print(f"Full")
-print(f"Remaining Dollars")
+print(f"Remanining Balance: $244")
+print(f"Full $20 Bills: 12")
+print(f"Remaining Dollars: $4")
